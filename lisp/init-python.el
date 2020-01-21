cea86e6097713606f7e638ae11c078c2b3256054
yuansl <yuanshenglong@qiniu.com>
2020-01-21 23:54:17 +0800

    replace jedi with pyls in python-mode

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,11 +1,4 @@
 (set-default 'python-shell-interpreter "python3")
 
-(require 'company-jedi)
-(add-to-list 'company-backends '(company-jedi))
-(add-hook 'python-mode-hook (lambda ()
-			      (setq jedi:use-shortcuts t); use key-binding 'M-.' and 'M-,'
-			      (setq jedi:environment-virtualenv
-				    (append python-environment-virtualenv
-					    '("--python" "python3")))
-			      (jedi:setup)))
+(add-hook 'python-mode-hook #'lsp)
 (provide 'init-python)
