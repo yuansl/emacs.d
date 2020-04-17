e2e30438aa433878216abfe766331c9c158d81ce
yuansl <yuanshenglong@qiniu.com>
2020-04-17 21:25:40 +0800

    refresh packages & ido use literal filename to find-file-at-point

--- init.el
+++ init.el
@@ -25,14 +25,14 @@
  '(ido-enable-flex-matching t)
  '(ido-enable-regexp t)
  '(ido-mode (quote both) nil (ido))
- '(ido-use-filename-at-point (quote guess))
+ '(ido-use-filename-at-point t)
  '(ido-use-url-at-point t)
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
  '(mouse-avoidance-mode (quote animate) nil (avoid))
  '(package-selected-packages
    (quote
-    (go-mode lsp-ui use-package company-lsp find-file-in-repository flycheck ack yaml-mode emojify company-c-headers markdown-mode async yasnippet sql-indent ggtags company)))
+    (go-mode lsp-ui use-package company-lsp find-file-in-repository flycheck ack yaml-mode emojify company-c-headers markdown-mode async yasnippet sql-indent company)))
  '(show-paren-mode t)
  '(size-indication-mode t)
  '(tool-bar-mode nil)
@@ -76,8 +76,6 @@
 (use-package company-lsp
   :commands company-lsp)
 
-(use-package ggtags)
-(use-package yasnippet)
 (use-package sql-indent
   :init
   (setq sql-indent-offset 8))
@@ -88,6 +86,11 @@
 (require 'init-c)
 (require 'init-go)
 (require 'init-python)
+
+(use-package yasnippet
+  :ensure t
+  :commands yas-minor-mode
+  :hook (go-mode . yas-minor-mode))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
