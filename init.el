834ef80fd0977cde066ae93b24e04213e143c1cc
yuansl <yuanshenglong@126.com>
2021-07-24 16:50:28 +0800

    delete package go-snippets, use yasnippet-snippets instead

--- init.el
+++ init.el
@@ -32,7 +32,7 @@
  '(large-file-warning-threshold 2000000)
  '(mouse-avoidance-mode 'animate nil (avoid))
  '(package-selected-packages
-   '(yasnippet-snippets which-key bui go-dlv editorconfig markdown-toc flycheck-kotlin kotlin-mode lsp lua-mode protobuf-mode go-snippets projectile-speedbar projectile go-impl scala-mode go-mode lsp-ui use-package find-file-in-repository flycheck yaml-mode company-c-headers markdown-mode yasnippet sql-indent company))
+   '(yasnippet-snippets which-key bui go-dlv editorconfig markdown-toc flycheck-kotlin kotlin-mode lsp lua-mode protobuf-mode projectile-speedbar projectile go-impl scala-mode go-mode lsp-ui use-package find-file-in-repository flycheck yaml-mode company-c-headers markdown-mode yasnippet sql-indent company))
  '(show-paren-mode t)
  '(size-indication-mode t)
  '(tool-bar-mode nil)
@@ -113,6 +113,11 @@
   :ensure t
   :mode ("\\.md\\'" . gfm-mode)
   :commands (markdown-mode gfm-mode))
+
+(use-package yasnippet
+  :config
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook #'yas-minor-mode))
 
 ;; load initialization for c programming language and html mode
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
