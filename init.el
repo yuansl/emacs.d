0bb38784b76ff97bad567cd1df49b9330e353d9f
yuansl <yuanshenglong@126.com>
2023-01-06 16:09:30 +0800

    add lsp-treemacs package

--- init.el
+++ init.el
@@ -48,7 +48,7 @@
  '(org-agenda-files nil)
  '(package-native-compile t)
  '(package-selected-packages
-   '(go-mode yasnippet markdown-mode use-package lsp-ui lsp-mode go-playground clang-format company-c-headers magit which-key company gotest yaml-mode sql-indent yasnippet-snippets bind-key markdown-toc helm))
+   '(lsp-treemacs 0blayout use-package lsp-ui lsp-mode go-playground clang-format company-c-headers magit which-key company gotest yaml-mode sql-indent yasnippet-snippets bind-key markdown-toc helm))
  '(save-place-mode t)
  '(size-indication-mode t)
  '(tool-bar-mode nil)
@@ -133,6 +133,10 @@
 				 (define-key lsp-ui-mode-map
 					     [remap xref-find-references] #'lsp-ui-peek-find-references)
 				 (define-key lsp-ui-mode-map (kbd "M-/") #'lsp-ui-peek-find-implementation))))
+
+(use-package lsp-treemacs
+  :config
+  (lsp-treemacs-sync-mode))
 
 ;; configuration for editing html/xhtml...
 (add-hook 'text-mode-hook
